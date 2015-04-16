b4615b04b5848a1addbf5cee9f39a48f207a19e5
MikhailUlyanov <ulyanov.misha@mail.ru>
2015-04-16 10:34:51 +0300

    13

---
+++ inventory/inventory.DesktopClient/Screens/CreateNewItems.lsml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewItems
+    {
+        partial void CreateNewItems_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Добавьте сюда свой код.
+            this.ItemsProperty = new Items();
+        }
+
+        partial void CreateNewItems_Saved()
+        {
+            // Добавьте сюда свой код.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.ItemsProperty);
+        }
+    }
+}
